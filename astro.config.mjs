2f2b64a58688a438c791182b8549b6879924d3f6
fuji620 <fujita.k.works@gmail.com>
2024-10-17 00:48:28 +0900

    追加

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  site: 'https://github.com/fuji620',
-  base: 'my-blog',
+  site: 'https://fuji620.github.io/',
+  base: '/my-blog/',
 });
